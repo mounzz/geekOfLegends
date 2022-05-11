7d10343743ce3880c147c91a7181974662d35b87
Mounzz <senmounia@hotmail.com>
2022-05-11 23:44:32 +0200

    combat ok, bugs à régler

--- public/js/boss.js
+++ public/js/boss.js
@@ -1,36 +1,36 @@
 class Boss {
     static riddles = [
         // enigme 1
-        { 
+        {
             question: "Qu'est-ce qui est vert et qui a la chair de poule",
             reponse: "un cornichon",
 
         },
         // enigme 2
-        { 
+        {
             question: "Qu'est-ce qui est jaune et qui attend?",
             reponse: "Jonathan",
 
         },
         // enigme 3
-        { 
+        {
             question: "Je commence la nuit et je termine le matin, qui suis-je ?",
             reponse: "N",
 
         },
     ];
-    constructor(nom, maxPv, pa ){
+    constructor(nom, maxPv, pa) {
         this.nom = nom;
         this.maxPv = this.currentPv = maxPv;
         this.pa = pa;
-        
+
     }
-    pickRiddles(){
-        const random = Math.floor(Math.random()* this.riddles.length)
-        return this.riddles[random];
+    pickRiddles() {
+        const random = Math.floor(Math.random() * Boss.riddles.length)
+        return Boss.riddles[random];
     }
-    attack(){
+    attack() {
         return this.pa;
     }
-    
+
 }
--- public/js/game.js
+++ public/js/game.js
@@ -1,167 +1,151 @@
-class Heroes{
-    constructor(nom, maxPv, pa){
+class Heroes {
+    constructor(nom, maxPv, pa) {
         this.nom = nom;
         this.maxPv = this.currentPv = maxPv;
         this.pa = pa;
     }
-    attack(){
+    attack() {
         this.pa *= 1.4;
         this.currentPv *= 0.75;
     }
-    defense(){
+    defense() {
         this.pa *= 0.5;
         this.currentPv *= 2.5;
     }
 }
 
-class Archer extends Heroes{
+class Archer extends Heroes {
     //carquois de flèches
     static quiver = [7, 8, 9, 10, 11];
-    constructor(nom, maxPv, pa){
+    constructor(nom, maxPv, pa) {
         super(nom, maxPv, pa);
         this.nbArrow = this.randomQuiver();
     }
-    
+
     //choisir en random le nombre de flèches
-    randomQuiver(){
-        const random = Math.floor(Math.random()*Archer.quiver.length);
+    randomQuiver() {
+        const random = Math.floor(Math.random() * Archer.quiver.length);
         return Archer.quiver[random];
     }
 
-    attack(){
+    attack() {
         super.attack();
-        if(this.nbArrow < 2){
+        if (this.nbArrow < 2) {
             this.nbArrow = 6;
-        } else{
-            this.nbArrow -=2;
+        } else {
+            this.nbArrow -= 2;
             return this.pa;
 
-        }     
+        }
 
     }
 }
 
-class Mage extends Heroes{
+class Mage extends Heroes {
     static mana = [7, 9, 11];
-    constructor(nom, maxPv, pa){
+    constructor(nom, maxPv, pa) {
         super(nom, maxPv, pa);
         this.manaPoints = this.randomMana();
     }
-    
 
-    randomMana(){
-        const random = Math.floor(Math.random()*Mage.mana.length);
+
+    randomMana() {
+        const random = Math.floor(Math.random() * Mage.mana.length);
         return Mage.mana[random];
     }
 
-    attack(){
+    attack() {
         super.attack();
-        if(this.manaPoints < 2){
+        if (this.manaPoints < 2) {
             this.manaPoints = 7;
-        } else{
-            this.manaPoints-=2;
+        } else {
+            this.manaPoints -= 2;
             return this.pa;
-        }     
+        }
     }
 }
 
-class Warrior extends Heroes{
-    constructor(nom, maxPv, pa){
+class Warrior extends Heroes {
+    constructor(nom, maxPv, pa) {
         super(nom, maxPv, pa)
         this.ragePoints = 0;
     }
 
-    attack(){
+    attack() {
         super.attack();
 
-        if(this.ragePoints === 4){
-            return this.pa *=1.25;
-        }else{
+        if (this.ragePoints === 4) {
+            return this.pa *= 1.25;
+        } else {
             return this.pa;
         }
 
     }
 }
 
-
 class Boss {
     static riddles = [
         // enigme 1
-        { 
+        {
             question: "Qu'est-ce qui est vert et qui a la chair de poule",
             reponse: "un cornichon",
 
         },
         // enigme 2
-        { 
+        {
             question: "Qu'est-ce qui est jaune et qui attend?",
             reponse: "Jonathan",
 
         },
         // enigme 3
-        { 
+        {
             question: "Je commence la nuit et je termine le matin, qui suis-je ?",
             reponse: "N",
 
         },
     ];
-    constructor(nom, maxPv, pa ){
+    constructor(nom, maxPv, pa) {
         this.nom = nom;
         this.maxPv = this.currentPv = maxPv;
         this.pa = pa;
-        
+
     }
-    pickRiddles(){
-        const random = Math.floor(Math.random()* this.riddles.length)
-        return this.riddles[random];
+    pickRiddles() {
+        const random = Math.floor(Math.random() * Boss.riddles.length)
+        return Boss.riddles[random];
     }
-    attack(){
+    attack() {
         return this.pa;
     }
-    
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //boucles pour poser questions
 
 let maxPoints = 300;
 
-const askQuestions = (question) =>{
+const askQuestions = (question) => {
     let userAnswer;
-    do{
+    do {
         userAnswer = +prompt(question);
     }
-    while(Number.isNaN(userAnswer) || userAnswer <= 0)
+    while (Number.isNaN(userAnswer) || userAnswer <= 0)
     return userAnswer;
 }
 
-const createHeroes = (heroClass) =>{
+const createHeroes = (heroClass) => {
     const nom = prompt(`Donnez un nom à votre ${heroClass}`);
     const maxPv = askQuestions(`Combien de points de vie lui accordez-vous ?`);
     const pa = askQuestions(`Combien de points d'attaque lui accordez-vous ?`);
     maxPoints -= (maxPv + pa);
     alert(`Il vous reste ${maxPoints} points à répartir`);
-    switch(heroClass){
+    switch (heroClass) {
         case 'archer':
-            return new Archer (nom, maxPv, pa);
+            return new Archer(nom, maxPv, pa);
         case 'warrior':
-            return new Warrior (nom, maxPv, pa);
+            return new Warrior(nom, maxPv, pa);
         case 'mage':
-            return new Mage (nom, maxPv, pa);
+            return new Mage(nom, maxPv, pa);
     }
 
 }
@@ -178,26 +162,26 @@ const pointsRepartition = () => {
         warrior = createHeroes('warrior')
         mage = createHeroes('mage')
     }
-    while(maxPoints > 0)
-    return {archer, warrior, mage}
+    while (maxPoints > 0)
+    return { archer, warrior, mage }
 
 }
 
 //choisir boss en random
 
 const selectBoss = () => {
-    const random = Math.floor(Math.random()*bosses.length);
+    const random = Math.floor(Math.random() * bosses.length);
     return bosses[random];
 }
 
 //determiner si heros sont KO
-const areHeroesDead = (heroes) =>{
-    return heroes.archer.currentPv <= 0 
-        && heroes.mage.currentPv <= 0 
+const areHeroesDead = (heroes) => {
+    return heroes.archer.currentPv <= 0
+        && heroes.mage.currentPv <= 0
         && heroes.warrior.currentPv <= 0
 }
 //determiner si boss est KO
-const isBossDead = (selectedBoss) =>{
+const isBossDead = (selectedBoss) => {
     return selectedBoss.currentPv <= 0;
 }
 
@@ -206,24 +190,28 @@ const chronos = new Boss("Chronos", 200, 20);
 const lilith = new Boss("Lilith", 300, 50);
 const sauron = new Boss("Sauron", 250, 30);
 const bosses = [chronos, lilith, sauron];
-const selectedBoss = selectBoss();
+// const selectedBoss = selectBoss();
+const selectedBoss = lilith;
 
 let isHeroesTurn = true;
 
-while(!isBossDead(selectedBoss) && !areHeroesDead(heroes)){
-    console.log(isBossDead, areHeroesDead)
+while (!isBossDead(selectedBoss) && !areHeroesDead(heroes)) {
     const bossTargets = [];
-    if(isHeroesTurn){
-        isHeroesTurn = !isHeroesTurn;
+    if (isHeroesTurn) {
 
         const userChoice = confirm(`Voulez-vous attaquer?`);
         console.log(userChoice)
 
-        if(userChoice){
+        if (userChoice) {
+            console.table(selectedBoss)
             selectedBoss.currentPv -= (heroes.archer.attack() + heroes.warrior.attack() + heroes.mage.attack());
+            console.table(selectedBoss)
+            if (selectedBoss.currentPv <= selectedBoss.maxPv * 0.2) {
+                selectedBoss.currentPv = selectedBoss.maxPv * 0.2;
+            }
 
-            heroes.warrior.ragePoints +=1;
-            heroes.archer.nbArrow +=1;
+            heroes.warrior.ragePoints += 1;
+            heroes.archer.nbArrow += 1;
 
         } else {
             heroes.archer.defense();
@@ -233,43 +221,43 @@ while(!isBossDead(selectedBoss) && !areHeroesDead(heroes)){
             bossTargets.push("archer", "archer", "warrior", "warrior", "mage", "mage")
         }
     } else {
-        if(selectedBoss.currentPv <= selectedBoss.maxPv * 0.2){
+        if (selectedBoss.currentPv <= selectedBoss.maxPv * 0.2) {
             const finalAttack = selectedBoss.pickRiddles();
-
+            let resolvedRiddle = false;
             let tentatives = 3;
-            let resolvedRiddle = false
-            while(tentatives > 0 || !resolvedRiddle){
+            while (tentatives > 0 && !resolvedRiddle) {
                 let userAnswer = prompt(finalAttack.question);
 
-                if(userAnswer === finalAttack.reponse){
+                if (userAnswer === finalAttack.reponse) {
                     resolvedRiddle = true;
-                    alert(`Vous avez vaincu le boss`)
+                    selectedBoss.currentPv = 0;
+                    alert(`Vous avez vaincu le boss`);
                 } else {
                     tentatives--;
                 }
-                if(tentatives === 0){
+                if (tentatives === 0) {
                     alert(`Vous avez perdu`);
                 }
             }
 
         } else {
-            const currentTarget = Math.floor(Math.random()*bossTargets.length);
+            const currentTarget = Math.floor(Math.random() * bossTargets.length);
 
-            switch(currentTarget){
+            switch (currentTarget) {
                 case 'archer':
                     heroes.archer.currentPv -= selectedBoss.pa
-                break;
+                    break;
                 case 'warrior':
                     heroes.warrior.currentPv -= selectedBoss.pa
-                break;
+                    break;
                 case 'mage':
                     heroes.mage.currentPv -= selectedBoss.pa
-                break;
+                    break;
             }
+            console.table(heroes)
+            console.table(currentTarget)
 
         }
     }
+    isHeroesTurn = !isHeroesTurn;
 }
-
-
-
